309b58685d43120f537001f4b8dc92f3b58be7c5
John <jrnoden@gmail.com>
2024-09-17 10:19:24 +0100

    Sets up vite project

---
+++ secret-santa/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function SecretSantaApp() {
+
+  return (
+    <>
+      <h1>Secret Santa App</h1>
+    </>
+  )
+}
+
+export default SecretSantaApp
